62945fa4e3fe61476e4f2ac1ead6a8da29ee0595
Bradley Lund <bradjlund@gmail.com>
2021-09-19 12:15:39 +0200

    set up dark mode toggle button

---
+++ components/DarkModeSwitch.js
@@ -0,0 +1,14 @@
+import { useColorMode } from "@chakra-ui/color-mode";
+import { IconButton } from "@chakra-ui/button";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+export default function DarkModeSwitch() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <IconButton
+      aria-label="Toggle Dark Switch"
+      icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+      onClick={toggleColorMode}
+    />
+  );
+}
