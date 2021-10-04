a464bba80084920c68ce0c14ca81922fa547c022
Bradley Lund <bradjlund@gmail.com>
2021-10-04 21:12:29 +0200

    add google analytics

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-4R9P5RGHWD"></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-4R9P5RGHWD');`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
